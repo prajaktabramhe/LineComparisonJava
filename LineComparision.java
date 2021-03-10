ecfc6d32c9fdaa757819c54df3a566a3477c9dc3
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-10 15:20:24 +0530

    [Prajakta]: Used OOPs concept to check equality and comparision

--- LineComparision.java
+++ LineComparision.java
@@ -1,49 +1,47 @@
- import java.util.Scanner;
- public class LineComparision {
-
-
-       public static void main(String args[]) {
-                //Declaration
-                double x1, x2, y1, y2, LengthofLine, LengthofLine2; 
-                
-                Scanner scan = new Scanner(System.in);
+import java.util.Scanner;
+public class LineComparision
+{
+	  //Declaration
+      public double x1, x2, y1, y2, LengthofLine, LengthofLine2; 
+      public void LengthofLine()
+      {
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("Enter the values for Line 1");
 	        System.out.println("Enter the value of x1");
-                //taking input from user
+	        //taking input from user for x1,x2,y1,y2 of Line1
 	        x1 = scan.nextDouble();
 	        System.out.println("Enter the value of x2");
-                //taking input from user
-                x2 = scan.nextDouble();
+	        x2 = scan.nextDouble();
 	        System.out.println("Enter the value of y1");
-                //taking input from user
-		y1 = scan.nextDouble();
-		System.out.println("Enter the value of y2");
-                //taking input from user
-                y2 = scan.nextDouble();
-
-		LengthofLine = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
-		System.out.println(LengthofLine);
-                
-                System.out.println("Enter the value of x1");
-                //taking input from user
-                x1 = scan.nextDouble();
-                System.out.println("Enter the value of x2");
-                //taking input from user
-                x2 = scan.nextDouble();
-                System.out.println("Enter the value of y1");
-                //taking input from user
-                y1 = scan.nextDouble();
-                System.out.println("Enter the value of y2");
-                //taking input from user
-                y2 = scan.nextDouble();
-
-                LengthofLine2 = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
-                System.out.println(LengthofLine);
-
-
-                String s1,s2;
-		s1=Double.toString(LengthofLine);
-		s2=Double.toString(LengthofLine2);
-	        System.out.println("Equality of two length is: " + s1.equals(s2));
-                System.out.println("Compare of two length is: " + s1.compareTo(s2));	
-      }
- }
+	        y1 = scan.nextDouble();
+	        System.out.println("Enter the value of y2");
+	        y2 = scan.nextDouble();
+	        LengthofLine = Math.sqrt((Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)));
+	        System.out.println("Length of Line 1: "+LengthofLine);
+	        
+	        System.out.println("Enter the values for Line 2");
+	        System.out.println("Enter the value of x1");
+	        //taking input from user  for x1,x2,y1,y2 of Line2
+	        x1 = scan.nextDouble();
+	        System.out.println("Enter the value of x2");
+	        x2 = scan.nextDouble();
+	        System.out.println("Enter the value of y1");
+	        y1 = scan.nextDouble();
+	        System.out.println("Enter the value of y2");
+	        y2 = scan.nextDouble();
+	
+	        LengthofLine2 = Math.sqrt((Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)));
+	        System.out.println("Length of Line 2: "+LengthofLine2);
+	
+	        String s1=Double.toString(LengthofLine);
+	        String s2=Double.toString(LengthofLine2);
+	        System.out.println("equality of two lines using Equal method " + s1.equals(s2));
+	        System.out.println("equality of two lines using CompareTo method " + s1.compareTo(s2));
+	}      
+	public static void main(String args[])
+	{
+			LineComparision lineobj1 = new LineComparision();
+			lineobj1.LengthofLine();
+		
+	}
+}
